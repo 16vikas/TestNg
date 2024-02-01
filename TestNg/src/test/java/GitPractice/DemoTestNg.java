2327355689c1375560c094fa7ccb414acb91add0
hp <hp@DESKTOP-BNPS8DG>
2024-02-01 16:13:17 +0530

    First Commit

---
+++ TestNg/src/test/java/GitPractice/DemoTestNg.java
@@ -0,0 +1,12 @@
+package GitPractice;
+
+import org.testng.annotations.Test;
+
+public class DemoTestNg {
+
+	@Test
+	public void dispatch() {
+		System.out.println("Dispatch Successfully");
+	}
+	
+}
